24d1eb7e775a43940bc400c4209476b60b772ca2
Leandro Schmitz <ssleandro@gmail.com>
2017-10-10 16:25:34 -0300

    #27: Fix Release Build with Optimization O3

--- mculib/mcu_src/ccan_18xx_43xx.c
+++ mculib/mcu_src/ccan_18xx_43xx.c
@@ -325,9 +325,10 @@ void Chip_CCAN_Send (LPC_CCAN_T *pCCAN, CCAN_MSG_IF_T IFSel, bool remoteFrame, C
 		//return;
 	}
 	Chip_CCAN_SetMsgObject(pCCAN, IFSel, CCAN_TX_DIR, remoteFrame, msgNum, pMsgObj);
-	while (Chip_CCAN_GetTxRQST(pCCAN) >> (msgNum - 1) && (wCount++ < 0x7FF))
+	while (Chip_CCAN_GetTxRQST(pCCAN) >> (msgNum - 1) && (wCount++ < 0x17FF))
 	{	// blocking , wait for sending completed
 	}
+
 	if (!remoteFrame)
 	{
 		freeMsgObject(pCCAN, IFSel, msgNum);
